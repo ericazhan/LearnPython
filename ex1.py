501ca5bae742b6afd8d195e67c25b591f3f32102
luyanne <34776378+luyanne@users.noreply.github.com>
2018-01-01 15:45:24 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print "hello world!"
+print "Hello Again"
+#print "I like typing this"
+print "This is fun"
+print "Yay! Printing."
+print "I'd much rther you 'not'."
+#print 'I "said" do not touch this.'
+print "i finished 'ex 1'."
+print 'i finished "ex 1".' #for the comments
+
