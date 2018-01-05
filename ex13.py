6ff831edd3b014b64bf8ce1f5aa07ab90a862cf2
luyanne <ericazhan123@gmail.com>
2018-01-06 00:13:26 +0100

    ex13 2nd version

--- ex13.py
+++ ex13.py
@@ -1,8 +1,8 @@
 from sys import argv
 
-script,first,second,third = argv
+aa,bb,cc,dd = argv
 
-print "The script is called:",script
-print "Your first variable is:",first
-print "Your second variable is",second
-print "Your third variable is:",third
+print "The first variable is called:",aa
+print "Your 2nd  is:",bb
+print "Your 3rd is",cc
+print "Your 4th is:",aa
