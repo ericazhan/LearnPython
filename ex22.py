91b29f0e6710401c1d49cce53baf781aee213366
luyanne <ericazhan123@gmail.com>
2018-01-19 23:21:36 +0100

    ex22

---
+++ ex22.py
@@ -0,0 +1,37 @@
+print " "
+#
+print " ",variable," "
+print " %r " %variable
+print " %r %s" %(variable1,variable2)
+%d
+%f
+%.4f
+print " " +" "
+print " ", #the function of this comma
+"""
+\n
+\t
+\t*
+\
+"\\" = "\"
++ - * / % < > <= >=
+raw_input()
+raw_input(" ")
+raw_input(variable)
+form sys import argv
+from os.path import exists
+exists( )
+fileobject = open( ）
+fileobject.read( )
+fileobject.open(" ","w")
+fileobject.truncate( )
+fileobject.write( )
+fileobject.close( )
+fileobject.seek( )
+len( )
++=
+def NameOfFunction(variable1,variable2):
+(tab)___________________
+def name(variable1,variable2):
+(tab)_______
+(tab)return
