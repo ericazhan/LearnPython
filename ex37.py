6ba57aa1c18b9735e591c70faab93e9ebb2de574
luyanne <ericazhan123@gmail.com>
2018-02-03 22:39:37 +0100

    ex37

---
+++ ex37.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 -*-
+#Learn 5 symbols each day 每天看5个
+#20180203
+#and,del,from,not,while
+
+mylist = [1,2,3,4,5,6,7,8,7,6,5,4,3]
+del mylist[4]
+print mylist
+del mylist
+#print mylist
+
+mylist = ["a","b","c","d"]
+mylist.pop(0)
+print mylist
