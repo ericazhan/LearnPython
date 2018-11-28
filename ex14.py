b6ec526f15fab969c9625e6e8a61721a05d713c9
linfengisep <wang.linfeng0213@gmail.com>
2018-11-28 23:10:43 +0100

    Update ex14.py

--- ex14.py
+++ ex14.py
@@ -20,4 +20,4 @@ print """
 Alright,so you said %r about liking me.
 You live in %r,Not sure where that is.
 And you have a %r computer. Nice.
-""" %(likes,lives,computer)
+""" %(likes,lives,computer)  #pas mal
